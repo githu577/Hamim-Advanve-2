20f5925d1d4eafdabef9b32d509601c55d5ade46
githu577 <github1234512345@gmail.com>
2026-08-13 23:17:13 +0600

    Update Fork.js

--- scripts/cmds/Fork.js
+++ scripts/cmds/Fork.js
@@ -18,13 +18,13 @@ module.exports = {
  ✦━━━━━━━━━✦
 👑 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗙𝗢𝗥𝗞 👑
 ✦━━━━━━━━━✦
-👑 𝗢𝗪𝗡𝗘𝗥 ➜ 𝗠𝗔𝗠𝗨𝗡
+👑 𝗢𝗪𝗡𝗘𝗥 ➜ 𝗛𝗔𝗠𝗜𝗠
 🤖 𝗕𝗢𝗧 ➜ 𝗚𝗢𝗔𝗧 𝗕𝗢𝗧 𝗩𝟮
 ━━━━━━━━━━━
 🌐 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗚𝗜𝗧𝗛𝗨𝗕
-🔗 https://github.com/MAMUN-GOAT-BOT/V2-.git
+🔗 https://github.𝗺𝘂𝗿𝗶𝗿_𝘀𝗮𝘁𝗲_𝗴𝗮𝗻𝗷𝗮_𝗺𝗶𝙨𝙞𝙮𝙚_𝗸𝗵𝗲𝘆𝗲_𝗺𝗼𝗿𝗲_𝗷𝗮𝘄_.𝗰𝗼𝗺
 ━━━━━━━━━━━
-🎥 𝗩𝗜𝗗𝗘𝗢 𝗧𝗨𝗧𝗢𝗥𝗜𝗔𝗟 📺 https://youtu.be/_Qb7Pq0Xavs?si=-MaOtPPnBPVSbIFX
+🎥 𝗩𝗜𝗗𝗘𝗢 𝗧𝗨𝗧𝗢𝗥𝗜𝗔𝗟 📺 https://vt.tiktok.com/ZSV1s2w27/
 ━━━━━━━━━━
 𝗠𝗔𝗠𝗨𝗡 𝗚𝗢𝗔𝗧 𝗕𝗢𝗧 𝗩𝟮
 ✦━━━━━━━━━✦
@@ -33,7 +33,7 @@ module.exports = {
  },
 
  onStart: async function ({ message, getLang }) {
- const link = "https://github.com/MAMUN-GOAT-BOT/V2-.git";
+ const link = "https://github.𝗺𝘂𝗿𝗶𝗿_𝘀𝗮𝘁𝗲_𝗴𝗮𝗻𝗷𝗮_𝗺𝗶𝙨𝙞𝙮𝙚_𝗸𝗵𝗲𝘆𝗲_𝗺𝗼𝗿𝗲_𝗷𝗮𝘄_.𝗰𝗼𝗺";
  return message.reply(getLang("current", link));
  },
 
@@ -41,7 +41,7 @@ module.exports = {
  const body = event.body?.trim().toLowerCase();
 
  if (body === "fork") {
- const link = "https://github.com/MAMUN-GOAT-BOT/V2-.git";
+ const link = "https://github.𝗺𝘂𝗿𝗶𝗿_𝘀𝗮𝘁𝗲_𝗴𝗮𝗻𝗷𝗮_𝗺𝗶𝙨𝙞𝙮𝙚_𝗸𝗵𝗲𝘆𝗲_𝗺𝗼𝗿𝗲_𝗷𝗮𝘄_.𝗰𝗼𝗺";
  return message.reply(getLang("current", link));
  }
  }
